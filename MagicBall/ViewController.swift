5b73627e8bcb08d23e538ace64bbb656cbaee0ac
Unknown <ingermanm@hotmail.com>
2018-04-18 18:06:21 -0600

    Ready to go

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   var puntero1 : Int = 0
+   let Arraybola = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var bola: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func MagicBall()
+    {
+        puntero1 = Int(arc4random_uniform(5))
+        bola.image = UIImage(named: Arraybola[puntero1])
+    }
+    
+    
+    @IBAction func boton(_ sender: UIButton) {
+        
+//        puntero1 = Int(arc4random_uniform(5))
+//        bola.image = UIImage(named: Arraybola[puntero1])
+        MagicBall()
+        
+    }
+    
+    // Shake or motion with phone
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        puntero1 = Int(arc4random_uniform(5))
+        bola.image = UIImage(named: Arraybola[puntero1])
+    }
 
 }
 
